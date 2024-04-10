d7edcdd3446b08c42f7929beecee03c4a3a0be26
Subhrojeet Dutta <106841956+geeksubhro@users.noreply.github.com>
2024-04-11 00:45:48 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+from typing import List 
+
+
+
+setup(
+    name = "GemsValuePipeline", 
+    version = "0.0.1", 
+    author = "Subhrojeet Dutta", 
+    description= "A trial End to End ML project created for First batch of LP students",
+    author_email= "Subhrojeet1232@gmail.com", 
+    packages=find_packages(),
+    )
